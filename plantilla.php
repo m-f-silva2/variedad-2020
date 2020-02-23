aaccd444a25c3699cb9d5693430c4f0dff342171
Manuel <mfsilva@unimayor.edu.co>
2020-02-23 16:00:33 -0500

    Diseño
    30%  completado.

---
+++ plantilla.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="text/html" charset="utf-8"/>
+    <meta name="keywords" content="PopaShop"/>
+    <meta name="description" content="Tienda Online"/>
+    <link type="text/css" rel="stylesheet" href="src/css/normalize.css"/>
+    <link type="text/css" rel="stylesheet" href="src/bootstrap/css/bootstrap.min.css"/>
+    <link type="text/css" rel="stylesheet" href="src/css/template.css"/>
+    <link type="text/css" rel="stylesheet" href="src/Ionicons/css/ionicons.min.css" />
+    <script src="src/bootstrap/js/jquery.min.js" type="text/javascript"></script>
+    <script src="src/bootstrap/js/bootstrap.js" type="text/javascript"></script>
+    <title>PopaShop</title>
+</head>
+<body>
+    <header>
+        <?php include_once "header.php";?>
+    </header>
+
+    <section>
+        
+    </section>
+
+    <footer>
+        <?php 
+            include_once ('modulos/footer.php');
+        ?>
+    </footer>
+</body>
+</html>
